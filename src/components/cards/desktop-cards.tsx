30a19d2ea7d4f385f1f1c9fd9a48f3ab223a7e1e
Swaraj Bachu <rajswaraj.r@gmail.com>
2024-09-01 14:01:36 +0530

    re adding the top close button

--- src/components/cards/desktop-cards.tsx
+++ src/components/cards/desktop-cards.tsx
@@ -4,7 +4,6 @@ import { X } from "lucide-react";
 import { useState, useRef, useEffect } from "react";
 import CoolButton from "../ui/cool-button";
 
-
 export default function DesktopCards({
   reveal,
   setReveal,
@@ -43,6 +42,16 @@ export default function DesktopCards({
       exit={{ opacity: 0 }}
       onClick={() => setSelected(null)}
     >
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          setReveal(false);
+        }}
+        className="absolute z-50 p-3 text-gray-200 bg-zinc-800 rounded-full cursor-pointer pointer-events-auto bg-opacity-70 backdrop-filter backdrop-blur-lg top-4 right-4"
+        aria-label="Close"
+      >
+        <X size={24} />
+      </button>
 
       <div className="relative w-full h-full flex justify-center items-center">
         {details.map((card, index) => (
@@ -66,133 +75,132 @@ export default function DesktopCards({
 }
 
 interface CardProps {
-    title: string;
-    content: string;
-    index: number;
-    isSelected: boolean;
-    setIsSelected: (index: number | null) => void;
-    totalCards: number;
-    details: string;
-    icon: React.ReactNode;
-    image: string;
-    website: string;
-  }
-  
-  function Card({
-    website,
-    icon,
-    image,
-    index,
-    title,
-    content,
-    isSelected,
-    setIsSelected,
-    totalCards,
-    details,
-  }: CardProps) {
-    const [dragging, setDragging] = useState(false);
-  
-    const centerIndex = Math.floor(totalCards / 2);
-    const offset = index - centerIndex;
-    const x = useRef(offset * 180);
-    const y = useRef(Math.abs(offset) * 20);
-  
-    const handleDragStart = () => {
-      setDragging(true);
-    };
-  
-    const handleDragEnd = (
-      _: MouseEvent | TouchEvent | PointerEvent,
-      info: PanInfo
-    ) => {
-      x.current = info.offset.x + x.current;
-      y.current = info.offset.y + y.current;
-      console.log(info.offset.x, x.current);
-      setDragging(false);
-    };
-  
-    const handleClick = (e: React.MouseEvent) => {
-      e.stopPropagation();
-      if (!dragging) {
-        setIsSelected(index);
-      }
-    };
-  
-    return (
+  title: string;
+  content: string;
+  index: number;
+  isSelected: boolean;
+  setIsSelected: (index: number | null) => void;
+  totalCards: number;
+  details: string;
+  icon: React.ReactNode;
+  image: string;
+  website: string;
+}
+
+function Card({
+  website,
+  icon,
+  image,
+  index,
+  title,
+  content,
+  isSelected,
+  setIsSelected,
+  totalCards,
+  details,
+}: CardProps) {
+  const [dragging, setDragging] = useState(false);
+
+  const centerIndex = Math.floor(totalCards / 2);
+  const offset = index - centerIndex;
+  const x = useRef(offset * 180);
+  const y = useRef(Math.abs(offset) * 20);
+
+  const handleDragStart = () => {
+    setDragging(true);
+  };
+
+  const handleDragEnd = (
+    _: MouseEvent | TouchEvent | PointerEvent,
+    info: PanInfo
+  ) => {
+    x.current = info.offset.x + x.current;
+    y.current = info.offset.y + y.current;
+    console.log(info.offset.x, x.current);
+    setDragging(false);
+  };
+
+  const handleClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    if (!dragging) {
+      setIsSelected(index);
+    }
+  };
+
+  return (
+    <motion.div
+      drag={!isSelected}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      initial={{ opacity: 0, y: 500, rotateY: -180 }}
+      animate={{
+        opacity: 1,
+        y: isSelected ? 0 : y.current,
+        rotateZ: isSelected
+          ? 0
+          : index === centerIndex + 1
+          ? 0
+          : -(index + 1 * 2),
+        height: isSelected ? "70vh" : 400,
+        width: isSelected ? 600 : 300,
+        zIndex: isSelected ? 100 : 50 + offset,
+        x: isSelected ? 0 : x.current,
+        rotateY: isSelected ? 0 : -180,
+        transition: {
+          delay: isSelected ? 0 : index * 0.1,
+          duration: 0.3,
+          type: "spring",
+          stiffness: 300,
+          damping: 30,
+        },
+      }}
+      style={{
+        perspective: "1200px",
+      }}
+      whileDrag={{ scale: 0.95, cursor: "grabbing" }}
+      onClick={handleClick}
+      className="absolute bg-gray-100 rounded-xl  shadow-xl flex flex-col items-center justify-center cursor-pointer"
+    >
       <motion.div
-        drag={!isSelected}
-        onDragStart={handleDragStart}
-        onDragEnd={handleDragEnd}
-        initial={{ opacity: 0, y: 500, rotateY: -180 }}
         animate={{
-          opacity: 1,
-          y: isSelected ? 0 : y.current,
-          rotateZ: isSelected
-            ? 0
-            : index === centerIndex + 1
-            ? 0
-            : -(index + 1 * 2),
-          height: isSelected ? "70vh" : 400,
-          width: isSelected ? 600 : 300,
-          zIndex: isSelected ? 100 : 50 + offset,
-          x: isSelected ? 0 : x.current,
-          rotateY: isSelected ? 0 : -180,
-          transition: {
-            delay: isSelected ? 0 : index * 0.1,
-            duration: 0.3,
-            type: "spring",
-            stiffness: 300,
-            damping: 30,
-          },
+          opacity: isSelected ? "0" : "1",
         }}
         style={{
-          perspective: "1200px",
+          rotateY: -180,
         }}
-        whileDrag={{ scale: 0.95, cursor: "grabbing" }}
-        onClick={handleClick}
-        className="absolute bg-gray-100 rounded-xl  shadow-xl flex flex-col items-center justify-center cursor-pointer"
+        transition={{
+          delay: isSelected ? 0 : 0.3,
+        }}
+        className="relative w-full h-full flex flex-col items-center justify-center"
       >
-        <motion.div
-          animate={{
-            opacity: isSelected ? "0" : "1",
-          }}
-          style={{
-            rotateY: -180,
-          }}
-          transition={{
-            delay: isSelected ? 0 : 0.3,
-          }}
-          className="relative w-full h-full flex flex-col items-center justify-center"
-        >
-          <h3 className="text-zinc-800 font-bold  mb-2 text-4xl">{title}</h3>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{
-            opacity: isSelected ? 1 : 0,
-          }}
-          exit={{ opacity: 0 }}
-          className="absolute inset-0 rounded-xl text-zinc-800 flex flex-col p-6  justify-between"
-        >
-          <div className="flex gap-2 items-center text-2xl font-bold">
-            {icon}
-            <h2>{title}</h2>
-          </div>
-          <div className="flex flex-col gap-2">
-            <video
-              src={image}
-              className="rounded-lg w-full h-72 object-cover"
-              autoPlay
-              loop
-            />
-  
-            <p className="text-lg text-zinc-800">{content}</p>
-          </div>
-          <CoolButton className="flex justify-center" href={website}>
-            visit
-          </CoolButton>
-        </motion.div>
+        <h3 className="text-zinc-800 font-bold  mb-2 text-4xl">{title}</h3>
       </motion.div>
-    );
-  }
-  
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: isSelected ? 1 : 0,
+        }}
+        exit={{ opacity: 0 }}
+        className="absolute inset-0 rounded-xl text-zinc-800 flex flex-col p-6  justify-between"
+      >
+        <div className="flex gap-2 items-center text-2xl font-bold">
+          {icon}
+          <h2>{title}</h2>
+        </div>
+        <div className="flex flex-col gap-2">
+          <video
+            src={image}
+            className="rounded-lg w-full h-72 object-cover"
+            autoPlay
+            loop
+          />
+
+          <p className="text-lg text-zinc-800">{content}</p>
+        </div>
+        <CoolButton className="flex justify-center" href={website}>
+          visit
+        </CoolButton>
+      </motion.div>
+    </motion.div>
+  );
+}
